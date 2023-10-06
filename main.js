95675135c775856219e7a76f3b025f6d9a4e7c89
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-06 21:06:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-setInterval(() => {
-    const time = document.querySelector("#time");
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let milliseconds = date.getMilliseconds();
-    time.textContent = hours + ":" + minutes;
-});
+//setInterval(() => {
+//    const time = document.querySelector("#time");
+//    let date = new Date();
+//    let hours = date.getHours();
+//    let minutes = date.getMinutes();
+//    let seconds = date.getSeconds();
+//    let milliseconds = date.getMilliseconds();
+//    time.textContent = hours + ":" + minutes;
+//});
+
+const time = document.querySelector("#time"); = {
+  format: (date: Date): string => {
+    const hours: string = T.formatHours(date.getHours()),
+          minutes: string = date.getMinutes(),
+          seconds: string = date.getSeconds();
+    
+    return `${hours}:${T.formatSegment(minutes)}`;
+  },
+  formatHours: (hours: number): string => {
+    return hours % 12 === 0 ? 12 : hours % 12;
+  },
+  formatSegment: (segment: number): string => {
+    return segment < 10 ? `0${segment}` : segment;
+  }
+}
