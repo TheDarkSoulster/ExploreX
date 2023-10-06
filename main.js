e0eb9e04b008f91d71a8f5462de94c03349e7632
Alt_Man <118615471+Kartik-bharadwaj@users.noreply.github.com>
2023-10-06 18:20:41 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    const time = document.querySelector("#time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let milliseconds = date.getMilliseconds();
+    time.textContent = hours + ":" + minutes + ":" + seconds;
+});
