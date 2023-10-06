d5cfded289cc2190172390d49cb82a3802aa2d53
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-06 21:25:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ setInterval(() => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes().toString().padStart(2, '0');
-    let period = hours >= 12 ? "ᴾᴹ" : "ᴬᴹ";
     
     // Convert hours to 12-hour format
     hours = (hours % 12) || 12;
