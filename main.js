91eaee644ff6290fc78ccf9144bce8fd472ca0dc
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-07 07:48:19 +0530

    Added Weather Temperature Display Functionality

--- main.js
+++ main.js
@@ -19,3 +19,25 @@ setInterval(() => {
     
     time.textContent = hours + ":" + minutes;
 });
+
+
+       const apiKey = 'c241d4e334702b5be0f81c3452c2960d';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&units=metric&appid=${apiKey}`;
+
+        async function getTemperature() {
+            try {
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+                const temperatureElement = document.getElementById('temperature');
+
+                if (data.main && data.main.temp) {
+                    temperatureElement.textContent = data.main.temp.toFixed(1);
+                } else {
+                    temperatureElement.textContent = 'N/A';
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        getTemperature();
