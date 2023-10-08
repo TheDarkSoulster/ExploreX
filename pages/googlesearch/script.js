09b2c0789e91fb694ae3a1d547abcefe9b3051c8
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-08 21:53:54 +0530

    Revamped & Added new Google Search UI w/ Bug Fixes

--- pages/googlesearch/script.js
+++ pages/googlesearch/script.js
@@ -1,33 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     var searchInput = document.getElementById('searchquery');
-    var datalist = document.getElementById('suggestions');
-
-    searchInput.addEventListener('input', function () {
-        var query = searchInput.value;
-        datalist.innerHTML = ''; // Clear previous suggestions
-
-        if (query.length > 0) {
-            // Fetch search suggestions from Google Autocomplete API
-            fetch(`https://suggestqueries.google.com/complete/search?client=chrome&q=${query}`)
-                .then(response => response.json())
-                .then(data => {
-                    var suggestions = data[1];
-                    suggestions.forEach(function (suggestion) {
-                        var option = document.createElement('option');
-                        option.value = suggestion;
-                        datalist.appendChild(option);
-                    });
-                })
-                .catch(error => {
-                    console.error('Error fetching suggestions:', error);
-                });
-        }
-    });
 
     searchInput.addEventListener('keydown', function (event) {
         if (event.keyCode === 13) {
-            var query = searchInput.value;
-            window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+            var userInput = searchInput.value;
+
+            // Check if the user input resembles a URL (ends with a valid domain)
+            var urlRegex = /^(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/[^\s]*)?$/;
+            if (urlRegex.test(userInput)) {
+                // If it's a URL, redirect the user to the website
+                var fullURL = userInput.startsWith('http') ? userInput : 'http://' + userInput;
+                window.location.href = fullURL;
+            } else {
+                // If it's not a URL, perform a Google search
+                var searchQuery = encodeURIComponent(userInput);
+                window.location.href = 'https://www.google.com/search?q=' + searchQuery;
+            }
         }
     });
 });
