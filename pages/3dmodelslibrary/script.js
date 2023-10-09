e8e88026e28b24f203b49dd043fec0a83ab06fac
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-09 21:10:50 +0530

    Update script.js

--- pages/3dmodelslibrary/script.js
+++ pages/3dmodelslibrary/script.js
@@ -1,84 +1,26 @@
-const modelsList = document.getElementById('modelsList');
-const searchInput = document.getElementById('searchInput');
-const itemsPerPage = 5;
-let currentPage = 1;
+function searchModels() {
+    const searchInput = document.getElementById('searchInput').value;
+    const modelList = document.getElementById('modelList');
+    modelList.innerHTML = ''; // Clear previous search results
 
-const SKETCHFAB_API_URL = 'https://api.sketchfab.com/v3/models';
-const SKETCHFAB_API_TOKEN = '25af9fac593d4fff869be23160fb9bb4'; // Sketchfab API token
+    // Fetch 3D models from Sketchfab API based on search input
+    fetch(`https://api.sketchfab.com/v3/search?type=models&q=${searchInput}`)
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(model => {
+                // Create a card for each 3D model
+                const modelCard = document.createElement('div');
+                modelCard.className = 'model-card';
+                modelCard.textContent = model.name;
+                
+                // Embed Sketchfab 3D viewer link on card click
+                modelCard.addEventListener('click', () => {
+                    window.open(`https://sketchfab.com/models/${model.uid}/embed`, '_blank');
+                });
 
-async function fetchSketchfabModels() {
-    try {
-        const response = await fetch(`${SKETCHFAB_API_URL}?token=${SKETCHFAB_API_TOKEN}&type=models`);
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        console.error('Error fetching Sketchfab models:', error);
-        return [];
-    }
+                // Add the model card to the model list
+                modelList.appendChild(modelCard);
+            });
+        })
+        .catch(error => console.error(error));
 }
-
-function displayModels(models, page) {
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const paginatedModels = models.slice(startIndex, endIndex);
-
-    modelsList.innerHTML = '';
-    paginatedModels.forEach(model => {
-        const modelCard = document.createElement('div');
-        modelCard.className = 'model-card';
-        modelCard.innerHTML = `
-            <h2>${model.name}</h2>
-            <iframe width="300" height="200" src="${model.embedUrl}" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-        `;
-        modelsList.appendChild(modelCard);
-    });
-}
-
-function filterAndPaginateModels() {
-    const searchTerm = searchInput.value.toLowerCase();
-    fetchSketchfabModels().then(sketchfabModels => {
-        const filteredModels = sketchfabModels.filter(model => model.name.toLowerCase().includes(searchTerm));
-        const totalPages = Math.ceil(filteredModels.length / itemsPerPage);
-
-        if (currentPage < 1) {
-            currentPage = 1;
-        } else if (currentPage > totalPages) {
-            currentPage = totalPages;
-        }
-
-        displayModels(filteredModels, currentPage);
-        renderPaginationControls(totalPages);
-    });
-}
-
-        displayModels(filteredModels, currentPage);
-        renderPaginationControls(totalPages);
-    });
-}
-
-function renderPaginationControls(totalPages) {
-    const paginationControls = document.createElement('div');
-    paginationControls.className = 'pagination-controls';
-    if (currentPage > 1) {
-        const prevButton = createPaginationButton('Previous', currentPage - 1);
-        paginationControls.appendChild(prevButton);
-    }
-    if (currentPage < totalPages) {
-        const nextButton = createPaginationButton('Next', currentPage + 1);
-        paginationControls.appendChild(nextButton);
-    }
-    modelsList.appendChild(paginationControls);
-}
-
-function createPaginationButton(label, targetPage) {
-    const button = document.createElement('button');
-    button.textContent = label;
-    button.addEventListener('click', () => {
-        currentPage = targetPage;
-        filterAndPaginateModels();
-    });
-    return button;
-}
-
-// Initial display of filtered and paginated models
-filterAndPaginateModels();
