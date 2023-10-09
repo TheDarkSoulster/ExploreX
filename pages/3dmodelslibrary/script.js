8f69466caeccb393558e05022509518180541b96
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-09 21:44:09 +0530

    Update script.js

--- pages/3dmodelslibrary/script.js
+++ pages/3dmodelslibrary/script.js
@@ -1,3 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch most viewed 3D models from Sketchfab API
+    fetch('https://api.sketchfab.com/v3/models?sort_by=-viewCount&type=models')
+        .then(response => response.json())
+        .then(data => {
+            const modelList = document.getElementById('modelList');
+
+            // Create cards for most viewed models
+            data.results.forEach(model => {
+                const modelCard = createModelCard(model);
+                modelList.appendChild(modelCard);
+            });
+        })
+        .catch(error => console.error(error));
+
+    // Function to create a model card
+    function createModelCard(model) {
+        const modelCard = document.createElement('div');
+        modelCard.className = 'model-card';
+
+        // Create title element
+        const title = document.createElement('div');
+        title.className = 'model-title';
+        title.textContent = model.name;
+        modelCard.appendChild(title);
+
+        // Create description element (limited to 150 characters with "..." if exceeded)
+        const description = document.createElement('div');
+        description.className = 'model-description';
+        description.textContent = model.description ? (model.description.length > 150 ? model.description.slice(0, 150) + '...' : model.description) : 'No description available';
+        modelCard.appendChild(description);
+
+        // Add "View Model" button
+        const viewModelButton = document.createElement('button');
+        viewModelButton.textContent = 'View Model';
+        viewModelButton.className = 'view-model-button';
+        viewModelButton.addEventListener('click', () => {
+            window.open(`https://sketchfab.com/models/${model.uid}/embed`, '_blank');
+        });
+        modelCard.appendChild(viewModelButton);
+
+        return modelCard;
+    }
+});
+
+
 function searchModels() {
     const searchInput = document.getElementById('searchInput').value;
     const modelList = document.getElementById('modelList');
