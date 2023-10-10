9d4f8643f4c3e3f123b7ad6f5dffe234ff41c525
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-10 20:42:15 +0530

    Update script.js

--- pages/Group Management/script.js
+++ pages/Group Management/script.js
@@ -1,4 +1,5 @@
-﻿const firebaseConfig = {
+// Initialize Firebase
+const firebaseConfig = {
     apiKey: "AIzaSyBSt-89RVn9gzK6aqlyd5sn0234GphPEKY",
     authDomain: "randomnamegenerator-explorex.firebaseapp.com",
     databaseURL: "https://randomnamegenerator-explorex.firebaseio.com",
@@ -8,41 +9,58 @@
     appId: "1:625852014663:web:417933b4705730c7825162"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
+// Function to Add Group
 function addGroup() {
     const groupName = document.getElementById("group-name").value;
-    const memberName = document.getElementById("member-name").value;
+    const membersInput = document.getElementById("member-name").value;
 
-    if (groupName && memberName) {
+    if (groupName && membersInput) {
+        const members = membersInput.split(",").map(member => member.trim());
+
         const groupRef = database.ref('groups/' + groupName);
-        groupRef.push().set({
-            member: memberName
+        groupRef.set({
+            members: members
         }).then(() => {
-            alert("Member added successfully!");
+            alert("Group added successfully!");
         }).catch(error => {
             console.error(error);
         });
     } else {
-        alert("Please enter group name and member name.");
+        alert("Please enter group name and member names.");
     }
 }
 
-function displayGroups() {
-    const groupList = document.getElementById("group-list");
-    const groupsRef = database.ref('groups');
-    groupsRef.on('value', snapshot => {
-        groupList.innerHTML = '';
-        snapshot.forEach(group => {
-            const groupName = group.key;
-            const members = group.val();
-            const listItem = document.createElement('li');
-            listItem.textContent = `${groupName}: ${Object.values(members).map(member => member.member).join(", ")}`;
-            groupList.appendChild(listItem);
+// Function to Delete Group
+function deleteGroup() {
+    const groupName = document.getElementById("group-name").value;
+    const groupRef = database.ref('groups/' + groupName);
+    groupRef.remove()
+        .then(() => {
+            alert("Group deleted successfully!");
+        })
+        .catch(error => {
+            console.error(error);
         });
-    });
 }
 
-displayGroups();
+// Function to Delete Member from a Group
+function deleteMember() {
+    const groupName = document.getElementById("group-name").value;
+    const memberId = document.getElementById("delete-member").value;
+
+    if (groupName && memberId) {
+        const groupRef = database.ref('groups/' + groupName + '/members/' + memberId);
+        groupRef.remove()
+            .then(() => {
+                alert("Member deleted successfully!");
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    } else {
+        alert("Please enter group name and member name to delete.");
+    }
+}
