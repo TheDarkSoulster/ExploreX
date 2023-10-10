25f88a1839a25e6fc9ecdcca1cbb6853b4ae8bec
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-10 16:19:22 +0530

    Update script.js

--- AppOverviews/TeamsOverview/script.js
+++ AppOverviews/TeamsOverview/script.js
@@ -1,17 +1,10 @@
 document.querySelector('.launch-app').addEventListener('click', function() {
-    // Check if the device is Android
+    // check if da device is Android cuz we don't use apple here. sorry, tim pressure cooker...
     if (/Android/i.test(navigator.userAgent)) {
-        try {
-            // Use Android Intents to launch Microsoft Teams
-            var intent = 'intent://com.microsoft.teams/.MainActivity#Intent;scheme=msteams;package=com.microsoft.teams;end;';
-            window.location.href = intent;
-        } catch (error) {
-            // Code to handle errors
-            console.error('Error launching Microsoft Teams:', error);
-            alert('Error launching Microsoft Teams.');
-        }
+        // take users to Google Play Store
+        window.location.href = 'https://play.google.com/store/apps/details?id=com.microsoft.teams';
     } else {
-        // Alert if Microsoft Teams ain't installed on the device, or it is used on another platform
-        alert('Microsoft Teams is not supported on this device.');
+        // we don't use apple here. too expensive
+        alert('Microsoft Teams is available on the Google Play Store. Please install it from there.');
     }
 });
