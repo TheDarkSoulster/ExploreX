b3ba853c8abd8b774c1f1c38c79dffdd6935902a
Saket Dhulipala <134040469+TheDarkSoulster@users.noreply.github.com>
2023-10-19 12:19:35 +0530

    Updated `script.js` to fix a bug

--- pages/3dmodelslibrary/script.js
+++ pages/3dmodelslibrary/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Fetch most viewed 3D models from Sketchfab API
-    fetch('https://api.sketchfab.com/v3/models?sort_by=-viewCount&type=models&features=SAFE')
+    fetch('https://api.sketchfab.com/v3/models?sort_by=-viewCount&type=models')
         .then(response => response.json())
         .then(data => {
             const modelList = document.getElementById('modelList');
@@ -13,37 +13,24 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error(error));
 
-    function searchModels() {
-        const searchInput = document.getElementById('searchInput').value;
-        const modelList = document.getElementById('modelList');
-        modelList.innerHTML = ''; // Clear previous search results
-
-        // Fetch 3D models from Sketchfab API based on search input and filter explicit content
-        fetch(`https://api.sketchfab.com/v3/search?type=models&q=${searchInput}&features=SAFE`)
-            .then(response => response.json())
-            .then(data => {
-                data.results.forEach(model => {
-                    const modelCard = createModelCard(model);
-                    modelList.appendChild(modelCard);
-                });
-            })
-            .catch(error => console.error(error));
-    }
-
+    // Function to create a model card
     function createModelCard(model) {
         const modelCard = document.createElement('div');
         modelCard.className = 'model-card';
 
+        // Create title element
         const title = document.createElement('div');
         title.className = 'model-title';
         title.textContent = model.name;
         modelCard.appendChild(title);
 
+        // Create description element (limited to 150 characters with "..." if exceeded)
         const description = document.createElement('div');
         description.className = 'model-description';
         description.textContent = model.description ? (model.description.length > 150 ? model.description.slice(0, 150) + '...' : model.description) : 'No description available';
         modelCard.appendChild(description);
 
+        // Add "View Model" button
         const viewModelButton = document.createElement('button');
         viewModelButton.textContent = 'View Model';
         viewModelButton.className = 'view-model-button';
